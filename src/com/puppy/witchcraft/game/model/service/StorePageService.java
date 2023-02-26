d4d3d32a730e38ff188e2aac33ba85a0c3bf5f05
HyoJeongNam <namgywjd2@gmail.com>
2023-02-26 17:45:25 +0900

    [feature] N-STORE
    playerGoldChange 수정해야 함
    inven 재료 구매시 카운트 넣어야 함

---
+++ src/com/puppy/witchcraft/game/model/service/StorePageService.java
@@ -0,0 +1,132 @@
+package com.puppy.witchcraft.game.model.service;
+
+import static com.puppy.witchcraft.common.Template.getSqlSession;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JLabel;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.puppy.witchcraft.game.model.dto.ImageDTO;
+import com.puppy.witchcraft.game.model.dto.ItemDTO;
+import com.puppy.witchcraft.game.model.dto.MyInven;
+import com.puppy.witchcraft.game.model.dto.PlayerDTO;
+import com.puppy.witchcraft.game.model.mapper.store.SqlMapper;
+
+public class StorePageService {
+
+	private static SqlMapper mapper;
+
+	//	public StorePageService(StorePageMapper storePageMapper) {
+	//		this.mapper = storePageMapper;
+	//	}
+
+	//	public List<Map<String, Object>> selectItemList() {
+	// 
+	//		SqlSession sqlSession = getSqlSession();
+	//		mapper = sqlSession.getMapper(StorePageMapper.class);
+	//		List<Map<String, Object>> itemList = mapper.selectItemList(sqlSession);
+	//		
+	//		sqlSession.close();
+	//		
+	//		return itemList;
+	//	}
+
+	public static List<ItemDTO> test() {
+
+		SqlSession sqlSession = getSqlSession();
+		mapper = sqlSession.getMapper(SqlMapper.class);
+
+		List<ItemDTO> itemList = mapper.storeList();
+
+		sqlSession.close();
+
+		return itemList;
+
+	}
+
+	public ImageDTO imageUrl(int imageNo) {
+
+		SqlSession sqlSession = getSqlSession();
+		mapper = sqlSession.getMapper(SqlMapper.class);
+
+		ImageDTO image = mapper.imageUrl(imageNo);
+
+		sqlSession.close();
+
+		return image;
+	}
+
+	public int insertstoreitem(ItemDTO item, PlayerDTO pp) {
+		
+		SqlSession sqlSession = getSqlSession();
+		mapper = sqlSession.getMapper(SqlMapper.class);
+
+		Map<String, Integer> inven = new HashMap<>();
+		inven.put("itemNo", item.getItemNo());
+		inven.put("playerNo", pp.getPlayerNo());
+
+		int result = mapper.insertstoreitem(inven);
+
+		if(result > 0) {
+			sqlSession.commit();
+		} else {
+			sqlSession.rollback();
+		}
+		sqlSession.close();
+
+		return result;
+	}
+
+	public List<MyInven> inven(int playerNo) {
+		
+		SqlSession sqlSession = getSqlSession();
+		mapper = sqlSession.getMapper(SqlMapper.class);
+		
+		List<MyInven> ivenitemList = mapper.myItemInven(playerNo);
+		
+		sqlSession.close();
+		
+		return ivenitemList;
+	}
+
+	public int playerGoldChange(JLabel playerGold, PlayerDTO pp) {
+		
+		SqlSession sqlSession = getSqlSession();
+		mapper = sqlSession.getMapper(SqlMapper.class);
+		
+		Map<String, Integer> gold = new HashMap<>();
+		gold.put("playerNo", pp.getPlayerNo());
+		gold.put("playerGold", pp.getPlayerGold());
+
+		int result = mapper.playerGoldChange(gold);
+
+		if(result > 0) {
+			sqlSession.commit();
+		} else {
+			sqlSession.rollback();
+		}
+		sqlSession.close();
+
+		return result;
+	}
+}
+
+//	public PlayerDTO playerGoldChange(PlayerDTO playerNo, ItemDTO itemNo) {
+//		
+//		SqlSession sqlSession = getSqlSession();
+//		mapper = sqlSession.getMapper(SqlMapper.class);
+//		
+//		PlayerDTO pp = mapper.playerGoldChange(playerNo, itemNo);
+//		
+//		sqlSession.close();
+//		
+//		return pp;
+//	}
+
+
+
+
